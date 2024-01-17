97f0c990602f1a881230fe2e8c74f59206c4c149
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-17 22:38:10 +0300

    pchar fixes

--- _push.c
+++ _push.c
@@ -13,7 +13,7 @@ void _push(stack_t **head, unsigned int count)
 
 	if (store.args)
 	{
-		if (sisdigit(store.args[b] == 0))
+		if (isdigit(store.args[b] == 0))
 			b++;
 		for (; store.args[b] != '\0'; b++)
 		{
