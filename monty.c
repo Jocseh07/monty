349a2b2a206ea17070389a6618d20320c819d5fd
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-17 20:02:40 +0300

    betty fixes

--- monty.c
+++ monty.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+__attribute((unused)) store_t store = {NULL, NULL, NULL, 0};
 
 /**
  * main - main function of the program
@@ -10,8 +11,6 @@
 int main(int ac, char **av)
 {
 	stack_t *stack = NULL;
-
-	__attribute((unused)) store_t store = {NULL, NULL, NULL, 0};
 
 	if (ac != 2)
 	{
