0249b77b3ed10b13151f08b034709a449c03b3a6
Joseph Muturi <josephmkaranja07@gmail.com>
2024-01-17 22:42:09 +0300

    push fixes

--- _push.c
+++ _push.c
@@ -13,7 +13,8 @@ void _push(stack_t **head, unsigned int count)
 
 	if (store.args)
 	{
-
+		if (isdigit(store.args[b]) == 0)
+			b++;
 		for (; store.args[b] != '\0'; b++)
 		{
 			if (isdigit(store.args[b]) == 0)
